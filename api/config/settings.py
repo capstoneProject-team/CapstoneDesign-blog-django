6f0d6a333132cd48c6b05a1cf9219f2646ad5b7b
CHAN HEE LEE <95417144+CHANHELEE@users.noreply.github.com>
2022-10-03 02:16:04 +0900

    [Feat] debugtoolbar 추가

--- api/config/settings.py
+++ api/config/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+INTERNAL_IPS = ['127.0.0.1']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- api/config/urls.py
+++ api/config/urls.py
@@ -24,8 +24,10 @@ urlpatterns = [
 
 ]
 
-# if settings.DEBUG:
-# 	import debug_toolbar
-# 	urlpatterns+=[
-# 		path('__debug__/', include(debug_toolbar.urls)),
-# 	] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
