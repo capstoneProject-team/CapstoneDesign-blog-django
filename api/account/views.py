945b613ef1d29cc944910107ab5e5c1056befcfa
CHAN HEE LEE <95417144+CHANHELEE@users.noreply.github.com>
2022-10-23 01:35:35 +0900

    [Feat] findPassword

--- api/account/views.py
+++ api/account/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView, UpdateAPIView ,DestroyAPIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.generics import CreateAPIView, UpdateAPIView ,DestroyAPIView,RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import User
 from .serializers import UserSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -35,11 +38,22 @@ class UpdateInfoView(UpdateAPIView):
     #     else:
     #         return Response({"message": "failed", "details": serializer.errors})
 
-
+#회원 삭제
 class DeleteView(DestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def FindPasswordView(request):
+    print(request.POST)
+    print(request.data['username'])
+    qs=User.objects.filter(username=request.data['username'],hint1=request.data['hint1'],hint2=request.data['hint2'])
+    serializer=UserSerializer(qs,many=True)
+    return Response(serializer.data)
 
+#JWT 토큰 Customizing
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
