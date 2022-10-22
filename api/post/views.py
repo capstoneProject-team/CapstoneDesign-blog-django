bcdabdc1d2d239c8f872db869d4a187ee5eacfbc
qkrdmswl <eunji303@naver.com>
2022-10-22 18:38:49 +0900

    [feat] filter customizing & page size to 5

--- api/post/views.py
+++ api/post/views.py
@@ -21,19 +21,9 @@ class PostListView(generics.ListCreateAPIView) :
     def list(self, request, *args, **kwargs):
         queryset = self.set_filters(self.get_queryset(), request)
 
-        """
-        paginate_queryset 내장 함수에 queryset 객채를 전달한다.
-        이때 paginator라는 페이징 클래스를 인스턴스화한 속성에
-        size_query_param을 "page_size"를 적용한다.
-        이는 한번에 보여줄 게시물의 갯수를 어떤 파라미터를 통해 전달하는지 설정하는 속성이다.
-        """
         self.paginator.page_size_query_param = "page_size"
         page = self.paginate_queryset(queryset)
 
-        """
-        page 객체가 존재한다면 get_paginated_response 내장함수를 이용해 응답한다.
-        200 Success로 응답한다.
-        """
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
@@ -42,14 +32,18 @@ class PostListView(generics.ListCreateAPIView) :
         return Response(serializer.data)
 
     def set_filters(self, queryset, request):
-        type = request.query_params.get('type', None)
-        description = request.query_params.get('description', None)
+        title = request.query_params.get('title', None)
+        content = request.query_params.get('content', None)
+        created_at = request.query_params.get('created_at', None)
 
-        if type is not None:
-            queryset = queryset.filter(type=type)
+        if title is not None:
+            queryset = queryset.filter(title__contains=title)
 
-        if description is not None:
-            queryset = queryset.filter(description__contains=description)
+        if content is not None:
+            queryset = queryset.filter(content__contains=content)
+
+        if created_at is not None:
+            queryset = queryset.filter(created_at__contains=created_at)
 
         return queryset
 
