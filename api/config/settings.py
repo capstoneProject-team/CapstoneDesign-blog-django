282b93ea71b03117c3323480511a8f582f4fa2ff
qkrdmswl <eunji303@naver.com>
2022-11-29 16:56:18 +0900

    chore: remove unused comments

--- api/config/settings.py
+++ api/config/settings.py
@@ -169,10 +169,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "account.User"
-# username 말고 email을 디폴트로 설정할 수 있는 방법?
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'ko-kr'
 
@@ -182,17 +178,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 INTERNAL_IPS = ['127.0.0.1']
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://3.36.254.187'
     , 'http://localhost:3000', 'http://www.harudiary.kr']
